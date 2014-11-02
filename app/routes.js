de323e5118940212bc23b645919ef6af90d48d91
Łukasz Gurdek <git@ukasiu.pl>
2014-11-02 11:27:08 +0100

    routes fixing

--- app/routes.js
+++ app/routes.js
@@ -4,7 +4,7 @@ angular.module('myApp').
     config(['$routeProvider', function ($routeProvider) {
         $routeProvider
             .when('/paremie', {
-                templateUrl: '/app/paremie/paremie.html',
+                templateUrl: 'app/paremie/paremie.html',
                 controller: 'paremie'
             })
             .otherwise({redirectTo: '/paremie'});
